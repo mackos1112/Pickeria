721c6948f623e7468e33ecde5a93a5928b1a74ab
mackos1112 <71689130+mackos1112@users.noreply.github.com>
2024-05-10 15:40:58 +0200

    Update Pickeria.cpp
    Minor Changes

--- Pickeria/Pickeria.cpp
+++ Pickeria/Pickeria.cpp
@@ -1,80 +1,175 @@
-include <iostream>
-#include <string>
+#include <iostream>
 #include <list>
+#include <string>
+
 using namespace std;
 
+class Skladnik {
+private:
+    string nazwaSkladnika;
+    string jednostka;
+    double cenaJednostkowa;
+    static int id;
+public:
+    string getNazwaSkladnika() {
+        return nazwaSkladnika;
+    }
 
+    string getJednostka() {
+        return jednostka;
+    }
+
+    double getCenaJednostkowa() {
+        return cenaJednostkowa;
+    }
+
+    int getID() {
+        return id;
+    }
+
+    void setNazwaSkladnika(string nazwa) {
+        this->nazwaSkladnika = nazwa;
+    }
+
+    void setJednostka(string jednostka) {
+        this->jednostka = jednostka;
+    }
+
+    void setCenaJednostkowa(double cena) {
+        this->cenaJednostkowa = cena;
+    }
+
+    Skladnik(string nazwa, string jednostkaZ, double cena) {
+
+        nazwaSkladnika = nazwa;
+        jednostka = jednostkaZ;
+        cenaJednostkowa = cena;
+
+        id++;
+
+    }
+
+    ~Skladnik() {
+
+    }
+
+};
+
+class ElementPosilku {
+private:
+    Skladnik** skladnik;  // pojedynczy skladnik
+    int ilosc;
+public:
+
+    void setIlosc(int number) {
+        this->ilosc = number;
+    }
+
+    int getIlosc() {
+        return ilosc;
+    }
+
+    ElementPosilku(int size) : ilosc(size) {
+        skladnik = new Skladnik * [size];
+    }
+
+
+    ~ElementPosilku() {
+        delete[] skladnik;
+    }
+};
+
+class Posilek {
+private:
+    list<Skladnik> listaSkladnikow;   //elementow posilku lista
+    double cena;
+
+public:
+    double getCena() {
+        return cena;
+    }
+
+    void setCena(double cenaZ) {
+        this->cena = cenaZ;
+    }                            //Konstruktor i destruktor
+};
+
+class ElementZamowienia {
+private:
+    Posilek posilek;
+    int ilosc;                    //Konstruktor i destruktor
+};
+
+int Skladnik::id = 0;
+
+
+
+class Zamowienie{	//placeholder zeby mi kompilator dziala
+public:
+    Zamowienie() {
+
+    }
+    ~Zamowienie() {
+
+    }
+
+private:
+
+};
+
+class Platnosc{	//parametry przeniesione z diagramu
+private:
+    int statusPlatnosci;	//mysle zeby statusy zrobic 0-9, obmnysle i napisze potem
+    float napiwek;
+    Zamowienie zamowienie;
+    int numerPlatnosci;
+
+public:
+    int zaplac() {
+        return 1;
+    }
+    void anulujPlatnosc() {
+        statusPlatnosci = 9;
+    }
+};
+
+class Gotowka{
+private:
+    float otrzymanaGotowka;
+
+public:
+    Gotowka() {
+
+    }
+    ~Gotowka() {
+
+    }
+
+};
+
+class Karta{
+private:
+    string nazwaKarty;
+    string typKarty;
+    string nazwaBanku;
+    int dataWaznosci;	//mozna potem zmienic na typ date
+
+public:
+    Karta() {
+
+    }
+    ~Karta() {
+
+    }
+    void autoryzacja() {
+
+    }
+
+};
 
 
 
 class Rezerwacja {
-
-
-
-using namespace std;
-
-class Zamowienie	//placeholder zeby mi kompilator dzialal
-{
-public:
-	Zamowienie();
-	~Zamowienie();
-
-private:
-
-};
-
-Zamowienie::Zamowienie()
-{
-}
-
-Zamowienie::~Zamowienie()
-{
-}
-class Platnosc	//parametry przeniesione z diagramu
-{
-private:
-	int statusPlatnosci;	//mysle zeby statusy zrobic 0-9, obmnysle i napisze potem
-	float napiwek;
-	Zamowienie zamowienie;
-	int numerPlatnosci;
-
-public:
-	int zaplac() {
-		return 1;
-	}
-	void anulujPlatnosc() {
-		statusPlatnosci = 9;
-	}
-};
-class Gotowka
-{
-public:
-	Gotowka() {
-
-	}
-	~Gotowka();
-	
-private:
-	float otrzymanaGotowka;
-};
-class Karta
-{
-public:
-	Karta() {
-
-	}
-	~Karta();
-	void autoryzacja();
-private:
-	string nazwaKarty;
-	string typKarty;
-	string nazwaBanku;
-	int dataWaznosci;	//mozna potem zmienic na typ date
-};
-
-
-class Skladnik {
-
 private:
     int numerRezerwacji;
 
@@ -123,7 +218,7 @@ private:
     int liczbaMiejsc;
     Rezerwacja tablicaRezerwacji[100];  //ile maksymalnie rezerwacji moze byc?
     int ostatniaRezerwacja = 0;             //numer elemntu tablicy pod ktorym jest ostatnia rezerwacja
-    
+
 
 
     static int iloscStolikow;
@@ -135,61 +230,36 @@ public:
         liczbaMiejsc = miejsca;
 
     }
-    
 
 
     bool zarezerwujStolik() {
         //wprowadzenie danych rezerwacji
-
-class ElementPosilku  {
-private:
-    Skladnik** skladnik;  // pojedynczy skladnik
-    int ilosc;
-
 
         int i = ++ostatniaRezerwacja;
         tablicaRezerwacji[i] = Rezerwacja();        //dac konstruktor wieloparametrowy
 
     }
 
-    void anulujRezerwacjeStolika(int numer){
+    void anulujRezerwacjeStolika(int numer) {
 
 
     }
-
 
     static void wyswietlWolneStoliki(int data, int godzina) {
 
-class Posilek {
-private:
-    list<Skladnik> listaSkladnikow;   //elementow posilku lista
-    double cena;
-
-
     }
 
-
-
-
-    void setCena(double cenaZ) {
-        this->cena = cenaZ;
-    }                            //Konstruktor i destruktor
-};
-
-class ElementZamowienia {
-private:
-    Posilek posilek;
-    int ilosc;                    //Konstruktor i destruktor
 
 };
 
 int Stolik::iloscStolikow = 0;
 
-int main()                    //Dane o posilkach i elementach posilku
-{
 
 
-     int wybor;
+
+int main() {
+	
+    int wybor;
     bool kierownik = false;
     cout << "MENU GLOWNE" << endl;
     cout << "1. Klient" << endl;
@@ -313,5 +383,7 @@ int main()                    //Dane o posilkach i elementach posilku
         break;
     }
 
-
+	
+	
+	return 0;
 }
