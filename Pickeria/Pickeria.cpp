a14f75297a3c44ca57e59d1e1fda6d6d292b4fc3
ibarteki26i@gmail.com <ibarteki26i@gmail.com>
2024-05-16 00:30:55 +0200

    Dodano rezerwacje stolika

--- Pickeria/Pickeria.cpp
+++ Pickeria/Pickeria.cpp
@@ -104,7 +104,7 @@ int Skladnik::id = 0;
 
 
 
-class Zamowienie{	//placeholder zeby mi kompilator dziala
+class Zamowienie {	//placeholder zeby mi kompilator dziala
 public:
     Zamowienie() {
 
@@ -117,7 +117,7 @@ private:
 
 };
 
-class Platnosc{	//parametry przeniesione z diagramu
+class Platnosc {	//parametry przeniesione z diagramu
 private:
     int statusPlatnosci;	//mysle zeby statusy zrobic 0-9, obmnysle i napisze potem
     float napiwek;
@@ -133,7 +133,7 @@ public:
     }
 };
 
-class Gotowka{
+class Gotowka {
 private:
     float otrzymanaGotowka;
 
@@ -147,7 +147,7 @@ public:
 
 };
 
-class Karta{
+class Karta {
 private:
     string nazwaKarty;
     string typKarty;
@@ -171,19 +171,21 @@ public:
 
 class Rezerwacja {
 private:
-    int numerRezerwacji;
+    string numerRezerwacji;
 
     string imieKlienta;
     string nazwiskoKlienta;
     string numerTelefonu;
-    int iloscOsob;          //czy potrzebne?
+    int iloscOsob;
 
-    int dataRezerwacji;
-    int godzinaRezerwacji;
-    int czasRezerwacji;     //jzk dlugo zajety
+    int dataRezerwacji;             //wstepnie dla uproszczenia data rezerwacji to dzien miesiaca
+    int godzinaRezerwacji;          //rezerwujemy tylko na pelne godziny(bez minut)
+    int czasRezerwacji;             //czas rezerwacji minimum 1h, calkowite godziny
 public:
     Rezerwacja(string imie, string nazwisko, string telefon, int osoby, int czas, int data, int godzina) {
         //generowanie numeru rezerwacji - albo data i godzina, albo inkrementacja poprzedniej rezerwacji
+        numerRezerwacji = imie.substr(0, 1) + nazwisko.substr(0, 1) + to_string(data) + "0" + to_string(godzina) + "o" + to_string(osoby);
+
         imieKlienta = imie;
         nazwiskoKlienta = nazwisko;
         numerTelefonu = telefon;
@@ -210,19 +212,35 @@ public:
         return numerTelefonu;
     }
 
+    string getRezerwacja() {
+        return numerRezerwacji;
+    }
+
+    bool czyKoliduje(int data, int godzina) {          //funckja pomocnicza abym nie musial pisac rozbudowanych warunkow
+        if (data != dataRezerwacji) return false;        //sprawdza czy podana data i godzina nie zachacza o jakas rezerwacje
+        else {                                          //uwzglednia czas rezerwacji
+            if (godzina >= godzinaRezerwacji && godzina <= (godzinaRezerwacji + czasRezerwacji)) return false;
+            else return true;                          //zwraca true gdy podane godziny koliduja z ta rezerwacja
+        }
+    }
+
 };
+
+
 
 class Stolik {
 private:
     int numerStolika;
     int liczbaMiejsc;
-    Rezerwacja tablicaRezerwacji[100];  //ile maksymalnie rezerwacji moze byc?
+
     int ostatniaRezerwacja = 0;             //numer elemntu tablicy pod ktorym jest ostatnia rezerwacja
 
 
 
     static int iloscStolikow;
 public:
+    Rezerwacja tablicaRezerwacji[100];  //ile maksymalnie rezerwacji moze byc?
+
 
     Stolik(int miejsca) {
         iloscStolikow++;
@@ -231,13 +249,36 @@ public:
 
     }
 
-
-    bool zarezerwujStolik() {
+    void zarezerwujStolik(int data, int godzina) {      //data i godzina sa podawane podczas wywietlania stolikow
         //wprowadzenie danych rezerwacji
+        string imie, nazwisko, telefon;
+        int czas, osoby;
+
+        cout << endl;
+        cout << "Podaj" << endl;
+        cout << "Imie: " << endl;
+        cin >> imie;
+        cout << "Nazwisko: " << endl;
+        cin >> nazwisko;
+        cout << "Numer telefonu: " << endl;
+        cin >> telefon;
+        do {
+            cout << "Jak dlugo: " << endl;
+            cin >> czas;
+            if (czas <= 0)cout << "Bledny czas, sprobuj jeszcze raz" << endl;
+        } while (czas <= 0);
+        do {
+            cout << "Ilosc osob: " << endl;
+            cin >> osoby;
+            if (osoby > liczbaMiejsc) cout << "Za duzo osob, sprobuj jeszcze raz" << endl;
+            if (osoby <= 0) cout << "Bledna ilsoc osob, sprobuj jeszcze raz" << endl;
+        } while (osoby > liczbaMiejsc || osoby <= 0);
 
         int i = ++ostatniaRezerwacja;
-        tablicaRezerwacji[i] = Rezerwacja();        //dac konstruktor wieloparametrowy
+        tablicaRezerwacji[i] = Rezerwacja(imie, nazwisko, telefon, osoby, czas, data, godzina);
 
+        cout << "Twoj numer Rezerwacji: " << endl;
+        cout << tablicaRezerwacji[i].getRezerwacja();
     }
 
     void anulujRezerwacjeStolika(int numer) {
@@ -245,25 +286,49 @@ public:
 
     }
 
-    static void wyswietlWolneStoliki(int data, int godzina) {
-
+    static int getIloscStolikow() { //getter, nie ma go w diagramie klas -gicu
+        return iloscStolikow;
     }
 
+    int getOstatnia() {
+        return ostatniaRezerwacja;
+    }
 
+    int getLiczbaMiejsc() {
+        return liczbaMiejsc;
+    }
 };
 
 int Stolik::iloscStolikow = 0;
 
 class SystemRestauracji {                       //singleton
 private:
-
+    Stolik stoliki[7] = { Stolik(6), Stolik(6), Stolik(4), Stolik(4), Stolik(5), Stolik(5), Stolik(5) };
+    //stoliki musza byc powiazane z systemem rezerwacji
 public:
     SystemRestauracji() {
 
     }
 
-    void WyswietlWolneStoliki() {
+    void WyswietlWolneStoliki(int data, int godzina) {
+        bool czy_wolny = true;
+        cout << "WOLNE STOLIKI" << endl;
+        for (int i = 0; i < Stolik::getIloscStolikow(); i++) {
+            for (int j = 0; j < stoliki[i].getOstatnia(); j++) {
+                if (stoliki[i].tablicaRezerwacji[j].czyKoliduje(data, godzina)) {
+                    czy_wolny = false;
+                    break;
+                }
+            }
+            if (czy_wolny) {
+                cout << i + 1 << ". Stolik numer " << i + 1 << endl;
+                cout << "Liczba miejsc: " << stoliki[i].getLiczbaMiejsc() << endl;
+            }
+        }
+    }
 
+    Stolik getStolikONr(int nr_stolika) {
+        return stoliki[nr_stolika - 1];
     }
 
     void WyswietlBiezaceMenu() {
@@ -299,7 +364,9 @@ public:
 
 
 int main() {
-	
+
+
+    SystemRestauracji* PICKERIA = new SystemRestauracji();
     int wybor;
     bool kierownik = false;
     cout << "MENU GLOWNE" << endl;
@@ -311,7 +378,7 @@ int main() {
 
     system("cls");
 
-    switch (wybor){
+    switch (wybor) {
     case 1:
         cout << "KLIENT" << endl;
         cout << "1. Zarezerwuj stolik" << endl;
@@ -328,7 +395,20 @@ int main() {
         {
         case 1:
             //Rezerwacja stolika
+            int data, godzina;
             cout << "Rezerwacja stolika" << endl;
+            cout << "Podaj date: " << endl;
+            cin >> data;
+            cout << "Podaj godzine: " << endl;
+            cin >> godzina;
+
+            PICKERIA->WyswietlWolneStoliki(data, godzina);
+
+            int wybor;
+            cout << "Wybierz stolik: " << endl;
+            cin >> wybor;
+
+            PICKERIA->getStolikONr(wybor).zarezerwujStolik(data, godzina);
             break;
         case 2:
             //Zamawianie posilku
@@ -352,8 +432,8 @@ int main() {
         break;
     case 2:
     case 3:
-        
-        if(wybor==2) cout << "PRACOWNIK" << endl;
+
+        if (wybor == 2) cout << "PRACOWNIK" << endl;
         else {
             cout << "KIEROWNIK" << endl;
             kierownik = true;
@@ -361,8 +441,8 @@ int main() {
 
         cout << "1. Przygotowanie zamowienia" << endl;
         cout << "2. Wydanie zamowienia" << endl;
-        cout << "3. Weryfikacja stanu magazynowego" << endl;        
-        cout << "4. Anuluj rezerwacje stolika" << endl;     
+        cout << "3. Weryfikacja stanu magazynowego" << endl;
+        cout << "4. Anuluj rezerwacje stolika" << endl;
         cout << "5. Anuluj zamowienie" << endl;
         cout << "6. Generuj raport kasowy" << endl;
 
@@ -424,7 +504,7 @@ int main() {
         break;
     }
 
-	
-	
-	return 0;
+
+
+    return 0;
 }
